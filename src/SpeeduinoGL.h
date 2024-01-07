1d08da1e186c159d73d0f57f691a4f79cc839647
Benjamin <88098139+Bexin3@users.noreply.github.com>
2024-01-07 16:14:42 +0100

    Add files via upload

--- src/SpeeduinoGL.h
+++ src/SpeeduinoGL.h
@@ -1,1 +1,32 @@
 
+#ifndef SpeeduinoGL_h
+#define SpeeduinoGL_h
+
+#include <Arduino.h>
+
+
+struct Point {
+  float w;
+  float h;
+};
+
+struct Triangle {
+  Point A;
+  Point B;
+  Point C;
+};
+
+struct Square {
+  Point A;
+  Point B;
+  Point D;
+  Point C;
+};
+
+void ConfigBuffer(long address, uint ResolutionV);
+void FillTriangle(Triangle triangle, uint16_t Colour);
+void FillSquare(Square square, uint16_t Colour);
+void PolarizedTwoLineRasterizer(int CellStartX, int CellEndX, float PointerCoordinateH, float PointerEndH, float Gradient1, float Gradient2, uint16_t Colour, bool Polarity);
+void TwoLineRasterizer(int CellStartX, int CellEndX, float PointerCoordinateH, float PointerEndH, float Gradient1, float Gradient2, uint16_t Colour);
+
+#endif
