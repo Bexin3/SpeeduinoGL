f33ea7017326763b53e24a95c9425b8a1e3fe0f0
Pleasant_exe <49663208+Pleasant-exe@users.noreply.github.com>
2024-01-08 02:52:44 +0000

    Removed unnecessary formatting SpeeduinoGL.cpp

--- src/SpeeduinoGL.cpp
+++ src/SpeeduinoGL.cpp
@@ -11,17 +11,12 @@ ImageAddress = address;
     ResH = ResolutionH;
 }
 
-
 void FillTriangle(Triangle triangle, uint16_t Colour) {
-
 
   // Sort points based on x-coordinates
   std::sort(&triangle.A, &triangle.C + 1, [](const Point &a, const Point &b) {
     return a.w < b.w;
   });
-
-
-
 
   float gradAC = (triangle.A.h - triangle.C.h) / (triangle.A.w - triangle.C.w);
   float gradAB = (triangle.A.h - triangle.B.h) / (triangle.A.w - triangle.B.w);
@@ -34,19 +29,14 @@ void FillTriangle(Triangle triangle, uint16_t Colour) {
 
     PolarizedTwoLineRasterizer(ceil(triangle.A.w), ceil(triangle.B.w), triangle.A.h + gradAC * (ceil(triangle.A.w) - triangle.A.w), triangle.A.h + gradAB * (ceil(triangle.A.w) - triangle.A.w), gradAB, gradAC, Colour, Polarized);
     PolarizedTwoLineRasterizer(ceil(triangle.B.w), ceil(triangle.C.w), triangle.A.h + gradAC * (ceil(triangle.B.w) - triangle.A.w), triangle.B.h + gradBC * (ceil(triangle.B.w) - triangle.B.w), gradBC, gradAC, Colour, Polarized);
-
 }
 
-
-
 void FillSquare(Square square, uint16_t Colour) {
-
 
   // Sort points based on x-coordinates
   std::sort(&square.A, &square.C + 1, [](const Point &a, const Point &b) {
     return a.w < b.w;
   });
-
 
 bool switched = 0;
 bool Polarized = 0;
@@ -54,8 +44,6 @@ bool Polarized = 0;
   float gradAC = (square.C.h - square.A.h) / (square.C.w - square.A.w);
    float gradAD = (square.D.h - square.A.h) / (square.D.w - square.A.w);
   float gradAB = (square.B.h - square.A.h) / (square.B.w - square.A.w);
-
-
 
   if (gradAD>gradAB) {
   if (gradAC>gradAD) {
@@ -73,7 +61,6 @@ switched = 1;
   };
   }
 
-
   float gradBC = (square.C.h - square.B.h) / (square.C.w - square.B.w);
   float gradDC = (square.C.h - square.D.h) / (square.C.w - square.D.w);
 
@@ -82,25 +69,16 @@ switched = 1;
   Polarized = 1;
  };
 
-
     PolarizedTwoLineRasterizer(ceil(square.A.w), ceil(square.B.w), square.A.h + gradAD * (ceil(square.A.w) - square.A.w), square.A.h + gradAB * (ceil(square.A.w) - square.A.w), gradAB, gradAD, Colour, Polarized);
-
-
-    if (switched) {
-
+    if (switched) 
+    {
     PolarizedTwoLineRasterizer(ceil(square.B.w), ceil(square.C.w), square.A.h + gradAD * (ceil(square.B.w) - square.A.w), square.B.h + gradBC * (ceil(square.B.w) - square.B.w), gradBC, gradAD, Colour, Polarized);
-
     PolarizedTwoLineRasterizer(ceil(square.C.w), ceil(square.D.w), square.A.h + gradAD * (ceil(square.C.w) - square.A.w), square.C.h + gradDC * (ceil(square.C.w) - square.C.w), gradDC, gradAD, Colour, Polarized);
-
-    } else {
-    
+    } else 
+    {
     PolarizedTwoLineRasterizer(ceil(square.B.w), ceil(square.D.w), square.A.h + gradAD * (ceil(square.B.w) - square.A.w), square.B.h + gradBC * (ceil(square.B.w) - square.B.w), gradBC, gradAD, Colour, Polarized);
-
     PolarizedTwoLineRasterizer(ceil(square.D.w), ceil(square.C.w), square.D.h + gradDC * (ceil(square.D.w) - square.D.w), square.B.h + gradBC * (ceil(square.D.w) - square.B.w), gradBC, gradDC, Colour, Polarized);
-
     };
-
-
 }
 
 void PolarizedTwoLineRasterizer(int CellStartX, int CellEndX, float PointerCoordinateH, float PointerEndH, float Gradient1, float Gradient2, uint16_t Colour, bool Polarity) {
@@ -111,7 +89,6 @@ void PolarizedTwoLineRasterizer(int CellStartX, int CellEndX, float PointerCoord
   };
 }
 
-
 void TwoLineRasterizer(int CellStartX, int CellEndX, float PointerCoordinateH, float PointerEndH, float Gradient1, float Gradient2, uint16_t Colour) {
 
 uint16_t *ImageBuffer = (uint16_t *)ImageAddress;
@@ -121,14 +98,10 @@ uint16_t *ImageBuffer = (uint16_t *)ImageAddress;
         PointerEndH -= Gradient1 * CellStartX;
         CellStartX = 0;
     };
-      
-      
   if (CellEndX > ResH) {
       CellEndX = ResH;
   };
-
   for (uint CurrentW = CellStartX; CellEndX > CurrentW; CurrentW++) {
-
     uint PointerCoorInt = ceil(PointerCoordinateH);
     uint PointerEndInt = ceil(PointerEndH);
       
@@ -139,11 +112,8 @@ uint16_t *ImageBuffer = (uint16_t *)ImageAddress;
     for (int CurrentH = PointerCoorInt; PointerEndInt > CurrentH; CurrentH++) {
       ImageBuffer[ResV * (CurrentW) + (CurrentH)] = Colour;
     };
-
-
+      
     PointerCoordinateH += Gradient2;
     PointerEndH += Gradient1;
   };
-
-
 }
