c7e290190bbdfaa971b08ee2f68c4a1b21433447
Pleasant_exe <49663208+Pleasant-exe@users.noreply.github.com>
2024-01-08 02:53:53 +0000

    Removed unnecessary formatting

--- examples/RotatingSquare/RotatingSquare.ino
+++ examples/RotatingSquare/RotatingSquare.ino
@@ -3,12 +3,9 @@
 #include "dsi.h"
 #include "SDRAM.h"
 
-
-
 uint16_t *FrameBuffer = (uint16_t *)SDRAM_START_ADDRESS;
 const int ResV = 480;
 float rotation = 0;
-
 
   Square sq1 = {
   {0, 0},
@@ -22,8 +19,6 @@ float angle = 0;
 // The buffer used to rotate and resize the frame
 Arduino_H7_Video Display(800, 480, GigaDisplayShield);
 
-
-
 void setup() {
   SDRAM.begin();
   Display.begin();
@@ -33,13 +28,8 @@ void setup() {
   dsi_lcdClear(0);
   dsi_drawCurrentFrameBuffer();
   ConfigBuffer(SDRAM_START_ADDRESS, ResV);
-
-
 }
-
 void loop() {
-
-
 
   // put your main code here, to run repeatedly:
 FillSquare(sq1, 0x0986);
@@ -52,19 +42,10 @@ FillSquare(sq1, 0x0986);
   {400 + 150 * (+ cos(angle) - sin(angle)), 240 + 150 * (- cos(angle) - sin(angle))},
   {400 + 150 * (+ cos(angle) + sin(angle)), 240 + 150 * (cos(angle) - sin(angle))}};
 
-
-
   long t1 = micros();
                                                              
-
-
 FillSquare(sq1, 0x00FF);
-
-
   Serial.println(micros() - t1);
   dsi_lcdDrawImage((void *)FrameBuffer, (void *)dsi_getCurrentFrameBuffer(), 480, 800, DMA2D_INPUT_RGB565);
   dsi_drawCurrentFrameBuffer();
-
-
 }
-
