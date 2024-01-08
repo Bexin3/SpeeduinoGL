8ce29a67fce4ea411ac31656cef07a57b78e54ad
Benjamin <88098139+Bexin3@users.noreply.github.com>
2024-01-08 15:37:46 +0100

    Add circle filling functionality and an example

---
+++ SpeeduinoGL/examples/RotatingSquare/RotatingSquare.ino
@@ -0,0 +1,51 @@
+#include "SpeeduinoGL.h"
+#include "Arduino_H7_Video.h"
+#include "dsi.h"
+#include "SDRAM.h"
+
+uint16_t* FrameBuffer = (uint16_t*)SDRAM_START_ADDRESS;
+const int ResV = 480;
+
+Rectangle sq1 = {
+  {0, 0},
+  {0, 480},
+  {800, 0},
+  {800, 480}
+};
+
+float angle = 0;
+
+// The buffer used to rotate and resize the frame
+Arduino_H7_Video Display(800, 480, GigaDisplayShield);
+
+void setup() {
+  SDRAM.begin();
+  Display.begin();
+  
+  dsi_lcdClear(0);
+  dsi_drawCurrentFrameBuffer();
+  dsi_lcdClear(0);
+  dsi_drawCurrentFrameBuffer();
+
+  ConfigBuffer(SDRAM_START_ADDRESS, ResV);
+}
+
+void loop() {
+  FillRectangle(sq1, 0x0986);
+
+  angle += 0.01;
+
+  sq1 = {
+    {400 + 150 * (-cos(angle) + sin(angle)),  240 + 150 * (cos(angle)  + sin(angle)) },
+    {400 + 150 * (-cos(angle) - sin(angle)),  240 + 150 * (-cos(angle) + sin(angle)) },
+    {400 + 150 * (cos(angle)  - sin(angle)),  240 + 150 * (-cos(angle) - sin(angle)) },
+    {400 + 150 * (cos(angle)  + sin(angle)),  240 + 150 * (cos(angle)  - sin(angle)) }
+  };
+
+  int32_t t1 = micros();
+  FillRectangle(sq1, 0x00FF);
+  Serial.println(micros() - t1);
+
+  dsi_lcdDrawImage((void *)FrameBuffer, (void *)dsi_getCurrentFrameBuffer(), 480, 800, DMA2D_INPUT_RGB565);
+  dsi_drawCurrentFrameBuffer();
+}
